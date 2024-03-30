8f88a48499106633ae7f2cf2d315a028ddcc8dbb
vladenich1 <vnichug202@yandex.ru>
2024-03-30 23:12:43 +0300

    new file:   vue/src/components/ui/ProgressBar.vue

---
+++ vue/src/components/ui/ProgressBar.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="progress-bar">
+    <div class="circle">
+      <div class="out-circle">
+        <div class="inner-circle">
+          <span class="value">{{ value }}%</span>
+        </div>
+      </div>
+      <svg class="progress-line" viewBox="0 0 36 36">
+        <path 
+          class="circle-bg"
+          d="M18 2.0845
+            a 15.9155 15.9155 0 0 1 0 31.8309
+            a 15.9155 15.9155 0 0 1 0 -31.8309"/>
+        <path 
+          class="circle-progress"
+          :style="{ strokeDasharray: circumference, strokeDashoffset: offset, stroke: colors[ Math.ceil((this.value / 10) - 1) ] }"
+          d="M18 2.0845
+            a 15.9155 15.9155 0 0 1 0 31.8309
+            a 15.9155 15.9155 0 0 1 0 -31.8309"/>
+      </svg>
+    </div>
+  </div>
+</template>
+    
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      offset: 0
+    };
+  },
+  watch: {
+    value(newValue) {
+      this.animateCircle(newValue)
+    }
+  },
+  mounted() {
+    this.animateCircle(this.value)
+  },
+  computed: {
+    colors () {
+      return [
+          '#3190cd', '#6975c8', '#8859bf', '#bf599e', '#de5769', '#df7447', '#f19730', '#f0b02e', '#80c15f', '#00d197'
+      ]
+    },
+    radius () {
+      return 16
+    },
+    circumference () {
+      return 2 * Math.PI * this.radius
+    }
+  },
+  methods: {
+    animateCircle(newValue) {
+      const progress = this.circumference - (newValue / 100) * this.circumference
+      this.offset = progress
+    }
+  }
+};
+</script>
+    
+<style>
+.progress-bar {
+  position: relative;
+}
+
+.circle {
+  position: relative;
+  width: 150px;
+  height: 150px;
+}
+
+.out-circle {
+  width: 125px;
+  height: 125px;
+  position: absolute;
+  top: 8%;
+  left: 8%;
+  background: #666567;
+  border-radius: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 5px 10px 2px rgb(0 0 0 / 46%);
+  z-index: 10;
+}
+
+.inner-circle {
+  background: #313133;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  box-shadow: 0px 5px 10px 2px rgb(21 21 21 / 58%) inset;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.value {
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 18px;
+}
+
+.progress-line {
+  position: absolute;
+}
+
+.circle-bg {
+  fill: none;
+  stroke: #202022;
+  stroke-width: 2;
+}
+
+.circle-progress {
+  fill: none;
+  stroke-width: 2;
+  stroke-linecap: round;
+  transition: stroke-dashoffset 0.5s;
+}
+</style>
+  
